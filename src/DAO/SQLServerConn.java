e9ff50f47bb9d906b1e0980437d7d0cfcb0d6819
311nguyenbaohuy <311nguyenbaohuy@gmail.com>
2019-10-17 17:37:48 +0700

    update query more clean

--- src/DAO/SQLServerConn.java
+++ src/DAO/SQLServerConn.java
@@ -41,34 +41,33 @@ public class SQLServerConn {
     // CHECK LOGIN, ENABLE ONLINE AND UPDATE IP_ADDR
     public static boolean Login(String userName, String password, String IP_addr) throws SQLException{
         Connection conn = getSQLServerConnection();
-        
-        Statement stmt = conn.createStatement();
-        String query = "SELECT * FROM dbo.USER_ACCOUNT WHERE User_Name = '" + userName + "' AND User_Password = " + password;
-        ResultSet rs = stmt.executeQuery(query);
-        if (rs.next()){
-            query = "UPDATE dbo.USER_ACCOUNT SET "
-                    + "IP_addr = '" + IP_addr + "', Status = 1 " + "WHERE User_Name = '" + userName + "'";
-            stmt.execute(query);
-        return true;
-        }
-        return false;
+        String query = "UPDATE dbo.USER_ACCOUNT SET IP_addr = ?, Status = 1 WHERE User_Name = ? AND User_Password = ?";
+        CallableStatement cstmt = conn.prepareCall(query);
+        cstmt.setString(1, IP_addr);
+        cstmt.setString(2, userName);
+        cstmt.setString(3, password);
+
+        return cstmt.executeUpdate() == 0;
   }
   
     // Logout
     public static void Logout(String userName) throws SQLException{
-      Connection conn = getSQLServerConnection();
-      Statement stmt = conn.createStatement();
-      String query = "UPDATE dbo.USER_ACCOUNT SET Status = 0 " + "WHERE User_Name = '" + userName + "'";
-      stmt.execute(query);
+        Connection conn = getSQLServerConnection();
+        String query = "UPDATE dbo.USER_ACCOUNT SET Status = 0 WHERE User_Name = ? ";
+        CallableStatement cstmt = conn.prepareCall(query);
+        cstmt.setString(1, userName);
+        cstmt.execute();
     }
 
     // CREATE ACCOUNT, INSERT TO DB
     public static boolean CreateAccount (String userName, String password) throws SQLException{
         Connection conn = getSQLServerConnection();
-        Statement stmt = conn.createStatement();
-        String query = "INSERT INTO dbo.USER_ACCOUNT (User_Name, User_Password) VALUES ('" + userName + "', '" + password + "')";
+        String query = "INSERT INTO dbo.USER_ACCOUNT (User_Name, User_Password) VALUES (?, ?)";
+        CallableStatement cstmt = conn.prepareCall(query);
+        cstmt.setString(1, userName);
+        cstmt.setString(2, password);
         try {
-            stmt.execute(query);
+            cstmt.execute();
             return true;
         } catch (Exception e) {
             return false;
@@ -79,8 +78,10 @@ public class SQLServerConn {
     public static User getUserByName(String userName) throws SQLException {
         Connection conn = getSQLServerConnection();
         Statement stmt = conn.createStatement();
-        String query = "SELECT * FROM dbo.USER_ACCOUNT WHERE User_Name = '" + userName +"'";
-        ResultSet rs = stmt.executeQuery(query);
+        String query = "SELECT * FROM dbo.USER_ACCOUNT WHERE User_Name = ?";
+        CallableStatement cstmt = conn.prepareCall(query);
+        cstmt.setString(1, userName);
+        ResultSet rs = cstmt.executeQuery();
         if (rs.next()){
             return new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
         }
@@ -92,16 +93,22 @@ public class SQLServerConn {
     // If insert successful return true else return fasle
     public static boolean createFriendRequest(String userName, String friendName) throws SQLException{
         Connection conn = getSQLServerConnection();
-        Statement stmt = conn.createStatement();
         User user = getUserByName(userName);
         User friend = getUserByName(friendName);
         
         int userID = user.getID();
         int friendID = friend.getID();
-
-        String query = "INSERT INTO dbo.FRIEND_REQUEST (User_Id, Friend_Id) VALUES (" + userID + "," + friendID + ")";
         
-        return stmt.execute(query);
+        String query = "INSERT INTO dbo.FRIEND_REQUEST (User_Id, Friend_Id) VALUES (?, ?)";
+        CallableStatement cstmt = conn.prepareCall(query);
+        cstmt.setInt(1, userID);
+        cstmt.setInt(2, friendID);
+        try {
+            cstmt.execute();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
     
     // Insert to FRIEND and Delete FriendRequest
@@ -165,6 +172,5 @@ public class SQLServerConn {
         String query = "DELETE FROM dbo.FRIEND_REQUEST WHERE USER_ID = " + userID + " AND Friend_Id = " + friendID;
         stmt.executeQuery(query); 
     }
-    
     // REMOVE request add friend.
 }
